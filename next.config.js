81c2451fc60e74243c72df81d319c3d80e94362e
nuttapat-mac <nuttapat.swd@gmail.com>
2024-01-19 01:49:39 +0700

    Update profile

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.shields.io',
+        port: '',
+        pathname: '/badge/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/drive-viewer/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
